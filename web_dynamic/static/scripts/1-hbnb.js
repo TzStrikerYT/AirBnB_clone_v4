3404ed8bdbf4690330e4b624050c76f648840f52
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-15 17:38:47 -0500

    [Add] Finish the task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,7 @@
 window.onload = function() {
-  const select = {}
-  const lista = []
+  let print = "";
+  const select = {};
+  const lista = [];
   $("DIV.amenities INPUT").click(function() {
     if ($(this).prop("checked") == true) {
       select[$(this).attr("data-id")] = $(this).attr("data-name");
@@ -10,10 +11,17 @@ window.onload = function() {
       for (let i = 0; i < lista.length; i++) {
 	const name = $(this).attr("data-name");
 	if (lista[i] == name)
-	  delete lista[i];
+	  lista.splice(i, 1);
       }
     }
+    const val4 = $("DIV.amenities h4").html();
+
+    if (val4.length >= 28 && val4.slice(val4.length - 3, val4.length) != "..."){
+      print = val4 + "..."
+      $("DIV.amenities h4").text(print);
+    } else if (val4.slice(val4.length - 3, val4.length) != "..."){
+      $("DIV.amenities h4").text(lista);
+    }
     console.log(lista);
-    $("DIV.amenities h4").text(lista);
   });
 }
