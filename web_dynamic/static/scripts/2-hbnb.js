d9ee2ffd471e92652e59ed5b94f2e2268860704b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-15 20:31:45 -0500

    [Add] Update the task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -29,6 +29,7 @@ window.onload = function() {
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function(data) {
     if (data["status"] === "OK") {
+      console.log(data["status"]);
       $("DIV#api_status").addClass("available");
     } else {
       $("DIV#api_status").removeClass("available");
